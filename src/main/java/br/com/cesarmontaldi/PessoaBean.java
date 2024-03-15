74cdc8ef19b9ad4ffeca1ac5fa019d51d458a074
Cesar <guto_montaldi@yahoo.com.br>
2024-03-15 11:04:25 -0300

    Mostrando imagem em tela.

--- src/main/java/br/com/cesarmontaldi/PessoaBean.java
+++ src/main/java/br/com/cesarmontaldi/PessoaBean.java
@@ -17,6 +17,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -323,6 +324,12 @@ public class PessoaBean {
 			e.printStackTrace();
 			getMsg("Erro ao consultar o cep!");
 		}
+	}
+	
+	public void downloadFoto() {
+		Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String fileDownloadId = params.get("fileDownloadId");
+		System.out.println(fileDownloadId);
 	}
 	
 }
